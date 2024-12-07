5d9a7a9cb49f693bbdca497416f320a58a71682b
Abhiram A R <abhiramajithr@gmail.com>
2024-12-07 14:40:41 +0530

    linke extactiion

---
+++ handle_automate.py
@@ -0,0 +1,57 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
+import re
+
+
+class HandleAutomation:
+    def __init__(self):
+        self._initialize_driver()
+
+    def _initialize_driver(self):
+        chrome_options = Options()
+        chrome_options.add_experimental_option(
+            "prefs",
+            {
+                "safebrowsing.enabled": True,
+            },
+        )
+        self.driver = webdriver.Chrome(options=chrome_options)
+
+    def extract_urls(self, subject: dict) -> dict:
+        url = self._create_url(subject)
+        try:
+            self.driver.get(url)
+            download_elements = self.driver.find_elements(
+                by=By.CLASS_NAME, value="elementor-button"
+            )
+            keys = [
+                element.find_element(By.CLASS_NAME, "elementor-button-text").text
+                for element in download_elements
+            ]
+            urls = [element.get_attribute("href") for element in download_elements]
+
+            gdrive_links = dict(zip(keys, urls))
+            for link in gdrive_links.keys():
+                gdrive_links[link] = self._download_link(gdrive_links[link])
+
+            print(gdrive_links)
+            self.driver.close()
+        except Exception as e:
+            print(e)
+
+    def _create_url(self, subject: dict) -> str:
+        sub_code = subject["code"]
+        sub_name = subject["name"]
+
+        url = f"https://www.ktunotes.in/ktu-{sub_code}-{sub_name}-notes/"
+
+        return url
+
+    def _download_link(self, link: str) -> str:
+        pattern = r"/d/([a-zA-Z0-9_-]+)"
+        match = re.search(pattern, link)
+        id = match.group(1)
+        url = f"https://drive.usercontent.google.com/u/0/uc?id={id}&export=download"
+
+        return url
