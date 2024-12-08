33e7dd3727c7c90b9d7a448dea968d46e1cd1f62
Abhiram A R <abhiramajithr@gmail.com>
2024-12-08 11:55:20 +0530

    resolver .env

---
+++ server/app.py
@@ -0,0 +1,18 @@
+from flask import Flask, request, jsonify
+from handle_automate import HandleAutomation
+import json
+
+app = Flask(__name__)
+
+
+@app.route("/api/data", methods=["POST"])
+def gdrive_links():
+    data = request.json
+    auto = HandleAutomation()
+    gdrive_url = auto.extract_urls(subject=data)
+
+    return jsonify(gdrive_urlss)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
--- server/handle_automate.py
+++ server/handle_automate.py
@@ -2,6 +2,7 @@ from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 import re
+import logging
 
 
 class HandleAutomation:
@@ -22,6 +23,7 @@ class HandleAutomation:
         url = self._create_url(subject)
         try:
             self.driver.get(url)
+            logging.info(f"Accessed URL: {url}")
             download_elements = self.driver.find_elements(
                 by=By.CLASS_NAME, value="elementor-button"
             )
@@ -32,26 +34,29 @@ class HandleAutomation:
             urls = [element.get_attribute("href") for element in download_elements]
 
             gdrive_links = dict(zip(keys, urls))
-            for link in gdrive_links.keys():
-                gdrive_links[link] = self._download_link(gdrive_links[link])
+            # for link in gdrive_links.keys():
+            #     gdrive_links[link] = self._download_link(gdrive_links[link])
 
-            print(gdrive_links)
-            self.driver.close()
+            return gdrive_links
         except Exception as e:
-            print(e)
+            logging.error(f"An error occurred: {e}")
+        finally:
+            self.driver.close()
 
     def _create_url(self, subject: dict) -> str:
+        if "code" not in subject or "name" not in subject:
+            raise ValueError("Subject dictionary must contain 'code' and 'name' keys")
         sub_code = subject["code"]
         sub_name = subject["name"]
 
         url = f"https://www.ktunotes.in/ktu-{sub_code}-{sub_name}-notes/"
-
         return url
 
     def _download_link(self, link: str) -> str:
         pattern = r"/d/([a-zA-Z0-9_-]+)"
         match = re.search(pattern, link)
+        if not match:
+            raise ValueError(f"Invalid Google Drive link: {link}")
         id = match.group(1)
         url = f"https://drive.usercontent.google.com/u/0/uc?id={id}&export=download"
-
         return url
